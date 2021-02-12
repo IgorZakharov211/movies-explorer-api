70de2851f46b9e650e70469bacc108df193c11f0
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-02-12 14:03:35 +0300

    feat: create users controllers

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,9 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const { JWT_SECRET } = require('../config');
 const AuthError = require('../errors/auth-error');
+const ConflictError = require('../errors/conflict-error');
+const ValidationError = require('../errors/validation-error');
+const NotFoundError = require('../errors/not-found-error');
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
@@ -21,6 +24,58 @@ const login = (req, res, next) => {
     });
 };
 
+const createUser = (req, res, next) => {
+  const { email, password, name } = req.body;
+  return bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      email, password: hash, name,
+    })
+      .then((user) => res.send({ data: user.toJSON() }))
+      .catch((err) => {
+        if (err.code === 11000) {
+          next(new ConflictError('Данный Email занят!'));
+        }
+        if (err.name === 'ValidationError') {
+          next(new ValidationError(`${Object.values(err.errors).map((error) => error.message).join(', ')}`));
+        } else {
+          next(err);
+        }
+      }));
+};
+
+const getUser = (req, res, next) => {
+  const { id } = req.params;
+  User.findById(id)
+    .orFail(() => new NotFoundError('Пользователь по заданному id не найден'))
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.kind === 'ObjectId') {
+        next(new ValidationError('Невалидний id'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+const updateProfile = (req, res, next) => {
+  const { email, name } = req.body;
+  const { id } = req.params;
+  User.findByIdAndUpdate(id, { email, name },
+    { new: true })
+    .orFail(() => NotFoundError('Пользователь по заданному id не найден'))
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.kind === 'ObjectId') {
+        next(new ValidationError('Невалидний id'));
+      }
+      if (err.name === 'ValidationError') {
+        next(new ValidationError(`${Object.values(err.errors).map((error) => error.message).join(', ')}`));
+      } else {
+        next(err);
+      }
+    });
+};
+
 module.exports = {
-  login,
+  login, createUser, getUser, updateProfile,
 };
